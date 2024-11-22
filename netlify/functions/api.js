f9d0288ef8093f04e5fd5acb176b353b951905f9
evilpixi <tatosalvi@gmail.com>
2024-11-22 18:10:14 -0300

    fixed the openai call

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-import OpenAI from 'openai-api';
+import OpenAI from 'openai';
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
